1fcd20820e58d1622fbc6ac599fb0c502cbd9103
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-07-04 17:39:01 +0700

    b3:oop trong ecma script

--- caculator.js
+++ caculator.js
@@ -12,9 +12,24 @@ class Caculator{
     tinhhieu = ()=>{
         return this.no1-this.no2
     }
+    tich = (a,b)=>a*b
 }
 const caculator = new Caculator(20,15)
 // console.log(caculator.no2);
 console.log(caculator.tinhtong(7,8));
 console.log(caculator.tinhtong2());
 console.log(caculator.tinhhieu());
+class Khonggian extends Caculator{
+    constructor(cao = 5){
+        super(35,20)
+        this.height = cao
+    }
+    thetich = ()=>{
+        return this.tich(10,5)*this.height
+    }
+}
+
+const khongian = new Khonggian()
+console.log(`khonggian`,khongian.tinhtong(10,30));
+console.log(`Thể tích`,khongian.tich(10,5)*khongian.height);
+console.log(`Thể tích với hàm`,khongian.thetich());
